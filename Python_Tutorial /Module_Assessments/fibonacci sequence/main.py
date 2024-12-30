1b2ba5d29503b03465d7cd0d1d73be0dfe436f34
ahmedhussienah99 <61842483+ahmedhussienah99@users.noreply.github.com>
2024-12-30 21:41:06 +0200

    Add files via upload

---
+++ Python_Tutorial /Module_Assessments/fibonacci sequence/main.py
@@ -0,0 +1,14 @@
+ 
+input_user=int(input("enter your number : "))
+count=1
+num1=0
+num2=1
+num3=1
+
+
+while count<=input_user:
+    print(num3)
+    num1=num2
+    num2=num3
+    num3=num1+num2
+    count=count+1
