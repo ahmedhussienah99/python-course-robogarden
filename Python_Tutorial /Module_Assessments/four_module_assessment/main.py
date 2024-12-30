79c3eb3cf5049434348f5cb102cce0197fceb4ee
ahmedhussienah99 <61842483+ahmedhussienah99@users.noreply.github.com>
2024-12-30 19:56:44 +0200

    Add files via upload

---
+++ Python_Tutorial /Module_Assessments/four_module_assessment/main.py
@@ -0,0 +1,5 @@
+radius=input("enter your radius : ")
+circumference=2*(int(radius)/2)*3.14
+area=3.14*(int(radius))*(int(radius))
+print("the circumference is : ",circumference)
+print("the area is : ",area)
